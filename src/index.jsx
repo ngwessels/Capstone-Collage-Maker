b785c4e2490b093dedcca1b8592b752056fdad5a
Isaac Hall and Ryan McLean <isaac+ryan@>
2019-04-15 13:55:06 -0700

    react template complete for now with needed dependencies. add dependencies as needed, lacking sass

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { AppContainer } from 'react-hot-loader';
+import { HashRouter } from 'react-router-dom';
 
 const render = (Component) => {
   ReactDOM.render(
     <AppContainer>
       <Component/>
+      </HashRouter>
     </AppContainer>,
     document.getElementById('react-app-root')
   );
--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,26 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(png|gif|jp(e*)g|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 8000,
+            name: 'images/[hash]-[name].[ext]'
+          }
+        }
+      },
+      {
+        test: /\.jsx?$/,
+        enforce: "pre",
+        loader: "eslint-loader",
+        exclude: /node_modules/,
+        options: {
+          emitWarning: true,
+          configFile: "./.eslintrc.json"
+          }
+        },
+        {
         test: /\.jsx?$/,
         loader: "babel-loader",
         exclude: /node_modules/,
@@ -41,7 +61,8 @@ module.exports = {
             "react",
           ],
           plugins: [
-            "react-hot-loader/babel"
+            "react-hot-loader/babel",
+            "styled-jsx/babel"
           ]
         }
       }
