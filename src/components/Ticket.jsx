a7bdec0782e0cbb510ef9b3d350d709f7d6d9aa4
Student <example@example.com>
2019-04-15 11:34:47 -0700

    barebones, no styles yet

---
+++ src/components/Ticket.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Ticket(props){
+  return (
+    <div>
+      <h3>{props.location} - {props.names}</h3>
+      <p><em>{props.issue}</em></p>
+      <hr/>
+    </div>
+  );
+}
+
+Ticket.propTypes = {
+  names: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  issue: PropTypes.string
+};
+
+export default Ticket;
---
+++ src/components/TicketList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Ticket from "./Ticket";
+
+function TicketList(){
+  return (
+    <Ticket
+      location="1A"
+      names="Thanos and Slim Shady's Daughter"
+      issue="Firebase will not save record!"
+      />
+  );
+}
+
+export default TicketList;
