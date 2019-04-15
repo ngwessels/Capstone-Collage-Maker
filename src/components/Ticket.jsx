802c083eb423f61f7bf2c5265235770154e83b8b
Student <example@example.com>
2019-04-15 11:46:04 -0700

    add inline styling

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Ticket(props){
+  let ticketStyles = {
+    backgroundColor: '#eee',
+    fontFamily: 'sans-serif',
+    paddingTop: '25x'
+  }
   return (
-    <div>
+    <div style={ticketStyles}>
       <h3>{props.location} - {props.names}</h3>
       <p><em>{props.issue}</em></p>
       <hr/>
