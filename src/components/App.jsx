bcbce67e0d722d0faffcd1d4f7a38d7a32247e6b
Nate Wessels <nwessels16@gmail.com>
2019-04-19 21:28:44 -0700

    worked on getting average pixel color

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,77 @@ import Error404 from './Error404';
 import image from "../assets/images/wilderness.jpg";
 
 
-function test(e) {
+function main(e) {
+  colorBlock(e);
+  const array = getColors();
+  changeColors(20);
+  getDominantColor(array, 20);
+}
+
+function getDominantColor(array, value) {
   let canvasGap = document.getElementById("myCanvas");
   var c = document.getElementById("myCanvas");
   var ctx = c.getContext("2d");
-  // var img = new Image();
-  // var img.src = "../assets/images/wilderness.jpg";
+  var img = document.getElementById("myPic");
+  var width = img.clientWidth;
+  var height = img.clientHeight;
+  let xCoord = 0;
+  let yCoord = 0;
+  let color = [];
+  let total = 0;
+  for(let y = 0; y < value; y++) {
+    for(let x = 0; x < value; x++) {
+      let instance = array[y][x];
+      let iLength = instance.length;
+      let red = instance[0];
+      let green = instance[1];
+      let blue = instance[2];
+      const colorLength = color.length;
+      let isClose = false;
+      for(let colorI = 0; colorI < colorLength; colorI++) {
+        if(red <= (color[colorI][0] - 100) && red >= (color[colorI][0] + 100)) {
+          let totalColor = color[colorI][3];
+          color[colorI][3] = totalColor + 1;
+          isClose = true;
+
+        }
+      }
+      if(color.length == 0 || isClose == false) {
+        color.push([red, green, blue, 0]);
+      }
+    }
+  }
+  console.log(color.length);
+  // for(let y = 0; y < value; y++) {
+  //   for(let x = 0; x < value; x++) {
+  //     let newRed = (averageRed / total);
+  //     let newGreen = (averageGreen / total);
+  //     let newBlue = (averageBlue / total);
+  //     console.log(newRed, newGreen, newBlue);
+  //     ctx.fillStyle = "rgba("+newRed+","+newGreen+","+newBlue+","+(255)+")";
+  //     ctx.fillRect( x, y, 1, 1 );
+  //   }}
+}
+
+function changeColors(value) {
+  let canvasGap = document.getElementById("myCanvas");
+  var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
+  var img = document.getElementById("myPic");
+  var width = img.clientWidth;
+  var height = img.clientHeight;
+  // for(let i = 1; i < height + 1; i++) {
+  //   for(let e = 1; e < width + 1; e++) {
+  //     ctx.fillStyle = "rgba("+0+","+0+","+0+","+(255)+")";
+  //     ctx.fillRect( e, i, 1, 1 );
+  //   }
+  // }
+}
+
+function colorBlock(e) {
+  let canvasGap = document.getElementById("myCanvas");
+  var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
   var img = document.getElementById("myPic");
   ctx.drawImage(img,1,1);
   let actualX = Math.floor(e.pageX - canvasGap.offsetLeft);
@@ -25,9 +90,6 @@ function test(e) {
   console.log(red, green, blue, alpha);
   let rgba = "rgba( " + red + ", " + green + ", " + blue + ", " + alpha + ")";
   document.getElementById("colorChoice").style.backgroundColor = rgba;
-  getColors();
-
-  // console.log(e);
 }
 
 function getColors() {
@@ -38,7 +100,7 @@ function getColors() {
   var img = document.getElementById("myPic");
   var width = img.clientWidth;
   var height = img.clientHeight;
-
+  console.log(width, height);
   for(let i = 1; i < height + 1; i++) {
     let row = [];
     for(let e = 1; e < width + 1; e++) {
@@ -50,25 +112,27 @@ function getColors() {
     array.push(row);
   }
   console.log(array);
+  return array;
 }
 
 let canvasStyle = {
 
-}
+};
 
 let imgStyle = {
 
-}
+};
 
 function App(){
   return (
     <div>
       <div>
         <div>
-          <canvas style={canvasStyle} onClick={test} id="myCanvas" ref="canvas" width={1000} height={900}/>
+          <canvas style={canvasStyle} onClick={main} id="myCanvas" ref="canvas" width={1000} height={900}/>
           <div style={{width: '100px', height: '100px'}} id="colorChoice"></div>
+          <div style={{width: '20px', height: '20px', backgroundColor: 'black'}}></div>
         </div>
-        <img onClick={() => this.test(someparameter)} />
+        <img onClick={() => this.main(someparameter)} />
         <img style={imgStyle} src={image} alt="" id="myPic"/>
     </div>
     </div>
