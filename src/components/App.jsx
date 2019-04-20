65dfae0c178d0785acd7c70818b48199a4a133a8
Nate Wessels <nwessels16@gmail.com>
2019-04-20 14:39:18 -0700

    fix any errors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,49 +2,58 @@ import React from 'react';
 import Header from './Header';
 import { Switch, Route } from 'react-router-dom';
 import Error404 from './Error404';
-import image from "../assets/images/luke.jpg";
+import image from "../assets/images/bird.jpg";
 
 
 function main(e) {
-  colorBlock(e);
-  const array = getColors();
-  let value = 5;
-  changeColors(value);
-  const blocks = getBlocks(array, value);
+  var img = document.getElementById("myPic");
+  var width = img.clientWidth;
+  var height = img.clientHeight;
+  let canvasGap = document.getElementById("myCanvas");
+  var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
+  const value = findBestValue(width, height);
+  // let value = 5;
+  colorBlock(e, canvasGap, c, ctx, img);
+  const array = getColors(canvasGap, c, ctx, img, width, height);
+  const blocks = getBlocks(array, value, width, height);
   const ylength = blocks.length;
   const xlength = blocks[0].length;
-  // let currentValue = blocks[1][595][0];
-  // console.log(currentValue);
   let total = 0;
   let lastY = 0;
   for(let i = 0; i < ylength; i++) {
-
     for(let e = 0; e < xlength; e++) {
       const current = blocks[i][e];
       const x = current[0];
       const y = current[1];
       total++;
-      if(blocks[i][e][0]) {
+      if(blocks[i][e]) {
         let currentValue = (blocks[i][e][0] + value);
         let yValue = blocks[i][e][1] + value;
-        getDominantColor(array, currentValue, x, y, yValue);
-        lastY = yValue;
-      } else {
+        getDominantColor(array, currentValue, x, y, yValue, width, height, canvasGap, c, ctx);
       }
-
     }
-
   }
-  console.log(total, lastY);
-
 }
 
-function getBlocks(array, value) {
+function findBestValue(width, height) {
+  let min = 3;
+  let isGood = false;
+  while(isGood == false) {
+    if(width % min == 0 && height % min == 0) {
+      isGood = true;
+
+    }
+    else {
+      min = min + 1;
+    }
+  }
+  return min;
+}
+
+function getBlocks(array, value, width, height) {
   let startX = 0;
   let startY = 0;
-  var img = document.getElementById("myPic");
-  var width = img.clientWidth;
-  var height = img.clientHeight;
   let area = (height * width) / value;
   let totalHeight = height / value;
   let totalWidth = width / value;
@@ -57,48 +66,54 @@ function getBlocks(array, value) {
     }
     blocks.push(row);
   }
-  console.log(blocks);
   return blocks;
 }
 
-function getDominantColor(array, value, xx, yy, yValue) {
-  let canvasGap = document.getElementById("myCanvas");
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
-  var img = document.getElementById("myPic");
-  var width = img.clientWidth;
-  var height = img.clientHeight;
+function getDominantColor(array, value, xx, yy, yValue, width, height, canvasGap, c, ctx) {
   let xCoord = 0;
   let yCoord = 0;
   let color = [];
   let total = 0;
   for(let y = yy; y < yValue; y++) {
     for(let x = xx; x < value; x++) {
-      if(!array[y][x]) {
-        console.log(x, y);
+      let instance;
+      if(array[y][x]) {
+        instance = array[y][x];
+      } else {
       }
-      let instance = array[y][x];
-      let iLength = instance.length;
-      let red = instance[0];
-      let green = instance[1];
-      let blue = instance[2];
-      const colorLength = color.length;
-      let isClose = false;
-      for(let colorI = 0; colorI < colorLength; colorI++) {
-        if(red >= (color[colorI][0] - 5) && red <= (color[colorI][0] + 5) && green >= (color[colorI][1] - 5) && green <= (color[colorI][1] + 5) && blue >= (color[colorI][2] - 5) && blue <= (color[colorI][2] + 5)) {
-          let totalColor = color[colorI][3];
-          color[colorI][3] = totalColor + 1;
-          isClose = true;
+      let iLength;
+      if(array[y][x]) {
+        iLength = instance.length;
+      } else {
+        iLength = 0;
+      }
+      if(iLength > 0) {
+        let red = instance[0];
+        let green = instance[1];
+        let blue = instance[2];
+        const colorLength = color.length;
+        let isClose = false;
+        for(let colorI = 0; colorI < colorLength; colorI++) {
+          if(red >= (color[colorI][0] - 5) && red <= (color[colorI][0] + 5) && green >= (color[colorI][1] - 5) && green <= (color[colorI][1] + 5) && blue >= (color[colorI][2] - 5) && blue <= (color[colorI][2] + 5)) {
+            let totalColor = color[colorI][3];
+            color[colorI][3] = totalColor + 1;
+            isClose = true;
 
+          }
         }
-      }
-      if(color.length == 0 || isClose == false) {
-        color.push([red, green, blue, 0]);
+        if(color.length == 0 || isClose == false) {
+          color.push([red, green, blue, 0]);
+        }
       }
     }
   }
   let colorLength = color.length;
   let longest = [];
+
+  changeColors(color, colorLength, longest, yy, xx, yValue, value, ctx);
+}
+
+function changeColors(color, colorLength, longest, yy, xx, yValue, value, ctx) {
   for(let i = 0; i < colorLength; i++) {
     let instance = color[i][3];
     if(longest.length == 0) {
@@ -117,29 +132,11 @@ function getDominantColor(array, value, xx, yy, yValue) {
       let newBlue = longest[2];
       ctx.fillStyle = "rgba("+newRed+","+newGreen+","+newBlue+","+(255)+")";
       ctx.fillRect( x, y, 1, 1 );
-    }}
+    }
+  }
 }
 
-function changeColors(value) {
-  let canvasGap = document.getElementById("myCanvas");
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
-  var img = document.getElementById("myPic");
-  var width = img.clientWidth;
-  var height = img.clientHeight;
-  // for(let i = 1; i < height + 1; i++) {
-  //   for(let e = 1; e < width + 1; e++) {
-  //     ctx.fillStyle = "rgba("+0+","+0+","+0+","+(255)+")";
-  //     ctx.fillRect( e, i, 1, 1 );
-  //   }
-  // }
-}
-
-function colorBlock(e) {
-  let canvasGap = document.getElementById("myCanvas");
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
-  var img = document.getElementById("myPic");
+function colorBlock(e, canvasGap, c, ctx, img) {
   ctx.drawImage(img,1,1);
   let actualX = Math.floor(e.pageX - canvasGap.offsetLeft);
   let actualY = Math.floor(e.pageY - canvasGap.offsetTop);
@@ -154,14 +151,8 @@ function colorBlock(e) {
   document.getElementById("colorChoice").style.backgroundColor = rgba;
 }
 
-function getColors() {
+function getColors(canvasGap, c, ctx, img, width, height) {
   let array = [];
-  let canvasGap = document.getElementById("myCanvas");
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
-  var img = document.getElementById("myPic");
-  var width = img.clientWidth;
-  var height = img.clientHeight;
   for(let i = 1; i < height + 1; i++) {
     let row = [];
     for(let e = 1; e < width + 1; e++) {
