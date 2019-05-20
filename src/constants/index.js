77180d9bce45c7ea8f76ca71985e0785ad090fc8
Nate Wessels <nwessels16@gmail.com>
2019-05-20 15:41:35 -0700

    fix errors

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
-// import * as types from './ActionTypes';
-// import firebaseConfig from './firebaseConfig';
-//
-// export default {
-//   firebaseConfig: firebaseConfig,
-//   c: types
-// };
+import * as types from './ActionTypes';
+import firebaseConfig from './firebaseConfig';
+
+export default {
+  firebaseConfig: firebaseConfig,
+  c: types
+};
